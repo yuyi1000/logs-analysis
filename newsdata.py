7ae221394222d9e4a10ac18b6d7e26a98e7f0829
Yi <yy471014@ohio.edu>
2018-10-05 11:27:02 -0400

    rearrange code format to pass pycodestyle checker

--- newsdata.py
+++ newsdata.py
@@ -5,25 +5,33 @@ import psycopg2
 '''
 show popular articles of all time according to their view times
 '''
-def get_top_three_articles ():
+
+
+def get_top_three_articles():
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute("select title, views from articles, \
-                    (select path, count(*) as views from log group by path order by views desc) as rank \
+                    (select path, count(*) as views from log \
+                    group by path order by views desc) as rank \
                     where path like '%' || slug")
     results = cursor.fetchall()
     db.close()
     _show_results(cursor.description, results)
 
+
 '''
 show most popular article authors of all time
 '''
-def get_authors_rank ():
+
+
+def get_authors_rank():
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
     cursor.execute("select name, sum(count) as views from articles, authors, \
-                    (select path, count(*) from log group by path order by count desc) as rank \
-                    where path like '%' || slug and authors.id = author group by authors.id")
+                    (select path, count(*) from log \
+                    group by path order by count desc) as rank \
+                    where path like '%' || slug \
+                    and authors.id = author group by authors.id")
     results = cursor.fetchall()
     db.close()
     _show_results(cursor.description, results)
@@ -32,20 +40,25 @@ def get_authors_rank ():
 '''
 show days that more than 1% requests lead to errors
 '''
-def get_high_request_error_days ():
+
+
+def get_high_request_error_days():
     db = psycopg2.connect("dbname=news")
     cursor = db.cursor()
-    cursor.execute("select total.time, error.count / total.count::real as error_rate \
-                    from (select time::date, count(*) from log group by time::date) as total, \
-                    (select time::date,  count(*) from log where status = '404 NOT FOUND' \
+    cursor.execute("select total.time, error.count / total.count::real \
+                    as error_rate from (select time::date, count(*) from log \
+                    group by time::date) as total, \
+                    (select time::date,  count(*) from log \
+                    where status = '404 NOT FOUND' \
                     group by time::date order by time::date) as error \
-                    where total.time = error.time and error.count / total.count::real > 0.01")
+                    where total.time = error.time \
+                    and error.count / total.count::real > 0.01")
     results = cursor.fetchall()
     db.close()
     _show_results(cursor.description, results)
 
 
-def _show_results (description, results):
+def _show_results(description, results):
     widths = []
     columns = []
     tavnit = '|'
@@ -53,7 +66,8 @@ def _show_results (description, results):
 
     for index, cd in enumerate(description):
         # widths.append(max(cd[2], len(cd[0])))
-        widths.append(max(max([len(str(result[index])) for result in results]), len(cd[0])))
+        max_col_length = max([len(str(result[index])) for result in results])
+        widths.append(max(max_col_length, len(cd[0])))
         columns.append(cd[0])
 
     for w in widths:
@@ -66,6 +80,7 @@ def _show_results (description, results):
     for row in results:
         print(tavnit % row)
     print(separator)
+
 
 if __name__ == '__main__':
     get_top_three_articles()
